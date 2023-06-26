42e39aef5adc06e623fd26cfa91242ded67485e7
marco <marcoc68@gmail.com>
2023-06-26 12:57:56 -0300

    wip

--- src/main/java/basic/P045AdvancedJavaSingletonPattern.java
+++ src/main/java/basic/P045AdvancedJavaSingletonPattern.java
@@ -1,14 +1,6 @@
 // https://www.hackerrank.com/challenges/java-singleton/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 package basic;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-import java.lang.reflect.*;
-
-
 class Singleton{
 	public String str;
 	private static final Singleton me = new Singleton();
